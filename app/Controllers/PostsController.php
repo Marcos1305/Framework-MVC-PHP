6f801cd69f7808a1a60b0179a3362874fbf99dfb
marcos1305 <marcos1305pt@gmail.com>
2018-05-31 10:33:22 -0300

    Primeiros testes com o model Post

--- app/Controllers/PostsController.php
+++ app/Controllers/PostsController.php
@@ -9,9 +9,10 @@ use Core\Container;
 class PostsController extends BaseController
 {
     public function index(){
+        $this->setPageTitle('Posts');
         $model = Container::getModel("Post");
-        $posts = $model->All();
-        print_r($posts);
+        $this->view->posts = $model->All();
+        $this->renderView('posts/index', 'layout');
     }
 
     public function show($id, $request){
