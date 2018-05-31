d539fca991b10d7d2fa59a7df3fa1768847ebf16
marcos1305 <marcos1305pt@gmail.com>
2018-05-30 21:15:50 -0300

    Criando classe para configuração da database

---
+++ app/database.php
@@ -0,0 +1,22 @@
+<?php 
+
+return [
+    /*
+        Options (mysql, sqlite)
+    */
+    'driver' => 'mysql',
+    'sqlite' =>[
+        'host' => 'database.db'
+    ],
+    'mysql' => [
+        'host' => 'cursomvc',
+        'database' => 'posts',
+        'user' => 'root',
+        'password' => '',
+        'charset' => 'utf-8',
+        'collation' => 'utf8_unicode_ci'
+    ]
+
+];
+
+?>
